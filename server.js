9f1383c12339f828e1a025132797d62c3899f062
Magdalena Miszewska <mag.miszewska@gmail.com>
2018-06-22 18:12:36 +0200

    Init project, add task

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var fs = require('fs');
+var app = express();
+var stringifyFile;
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.get('/getNote', function (req, res) {
+    console.log('Otrzymałam żądanie GET');
+    fs.readFile('./test.json', 'utf8', function(err, data) {
+        if (err) throw err;
+        stringifyFile = data
+        res.send(data);
+    });
+});
+
+app.post('/updateNote/:note', function(req, res) {
+    console.log('Otrzymałam żądanie POST');
+	stringifyFile += req.params.note;
+	fs.writeFile('./test.json', stringifyFile, function(err) {
+		if (err) throw err;
+		console.log('file updated');
+	});
+});
+
+app.listen(3000);
+
+app.use(function (req, res, next) {
+    res.status(404).send('Page not found!')
+});
